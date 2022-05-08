96a698ff60f45f02fccc57392d569d49b9ba0b18
Wiktor1305 <91824446+Wiktor1305@users.noreply.github.com>
2022-05-08 13:57:01 +0200

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-"""com"""
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def index():
-   """com"""
-   return '<h1>Hello WSB! Greetings from Flask!</h1>'
-if __name__ == "__main__":
-   app.run(debug=True)
